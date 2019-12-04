cc51073445b1c4adc1fa9bcc330f0b9da520374b
Jer-jer <jerichoallan0@gmail.com>
2019-12-04 08:11:08 +0700

    wtf is this

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const userRouter = require('./routes/users')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname +'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.static(__dirname +'/public'))
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { 
@@ -20,8 +24,9 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+db.once('open', () => console.log('Successfully Connected to Mongoose Server'))
 
 app.use('/', indexRouter)
+app.use('/users', userRouter)
 
 app.listen(process.env.PORT || 3000)
